9fa4326d45dcd324ecaa64c981815c1a041160dd
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-12-12 10:04:04 +0100

    feat: create (template) gradient function

---
+++ collektive-lib/lib/src/commonMain/kotlin/simple/gradient/Gradient.kt
@@ -0,0 +1,9 @@
+package simple.gradient
+
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.share
+import it.unibo.collektive.aggregate.values
+import it.unibo.collektive.stdlib.collapse.min
+
+fun Aggregate<Int>.gradient(source: Boolean): Int =
+    TODO("Implement gradient exploiting position taken from unity")
